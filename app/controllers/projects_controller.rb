9e1ee88d6161687761b5bd3a651784b2771605ea
Kaiying Fu <kaiying.fu@gmail.com>
2019-09-03 15:19:11 +0800

    Initialize Projects Controller and actions: :index, :new, :create

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,32 @@
+class ProjectsController < ApplicationController
+  # before_action :set_projects, only: [:index]
+
+  def index
+    @projects = policy_scope(Project).order(created_at: :desc)
+  end
+
+  def new
+    @project = Project.new
+    authorize @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to new_map_path(@project)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  # def set_projects
+  #   @projects = Project.all
+  #   authorize @projects
+  # end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :photo)
+  end
+end
