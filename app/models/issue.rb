6a0df5a0d81a53fc5206f26cd86a127fe5de1f8c
Terence Hung <terence.hung86@gmail.com>
2019-09-03 14:38:48 +0800

    init category models and references

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,9 +1,12 @@
 class Issue < ApplicationRecord
   belongs_to :map
   belongs_to :project
-  has_many :issue_assignments, dependent: :destroy
-  has_many :project_solvers, through: :issue_assignments
+  has_many :issue_solvers, dependent: :destroy
+  has_many :project_solvers, through: :issue_solvers
+  has_many :messages, dependent: :destroy
   has_one :user, through: :project
+  has_many :categorizations, dependent: :destroy
+  has_many :sub_categories, through: :categorizations
 
   validates :title, presence: true
 end
---
+++ db/migrate/20190903055522_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.string :text
+      t.string :photo
+      t.references :issue, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
