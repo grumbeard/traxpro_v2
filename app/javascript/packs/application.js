840eb4cc2e70c2d5e4a09a13c7d49be73d9e659a
Kaiying Fu <kaiying.fu@gmail.com>
2019-09-10 13:16:50 +0800

    CSS for Issue New forms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,11 +13,25 @@ if (categories !== null) {
   categories.forEach((category) => {
     category.addEventListener('click', (event) => {
       const selectedCategory = event.currentTarget;
+      selectedCategory.classList.toggle('category-card-selected');
+      selectedCategory.classList.toggle('category-card');
       selectedCategoryId = selectedCategory.dataset['id'];
       filterSubCategories(selectedCategoryId);
     });
   });
 };
+
+const sliderBtnContainer = document.querySelector('.slider-btn-container');
+const titleField = document.getElementById('issue_title');
+const mapField = document.getElementById('issue_map_id');
+if ((titleField.value === "") || (mapField.value === "")) {
+  console.log('it is empty!');
+  sliderBtnContainer.insertAdjacentHTML('beforeend', "<a href='#form-slider' role='button' data-slide='next'><div class='emerald action-btn'><p>Continue</p><span class='sr-only'>Next</span></div>");
+}
+
+if ((titleField.value !== "") && (mapField.value !== "")) {
+  sliderBtnContainer.innerHTML = "";
+}
 
 if (document.querySelector('[data-issues]')) {
     const map_all_pins_checker = document.querySelector('[data-issues]').dataset.issues
