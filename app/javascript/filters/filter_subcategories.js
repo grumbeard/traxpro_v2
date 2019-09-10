1d6b23bd4d02b5b129b55c1a562f2b2a131b72cb
Kaiying Fu <kaiying.fu@gmail.com>
2019-09-10 17:00:56 +0800

    Add ability to select subcategories by card instead of checkbox

--- app/javascript/filters/filter_subcategories.js
+++ app/javascript/filters/filter_subcategories.js
@@ -8,7 +8,22 @@ const filterSubCategories = (selectedCategoryId) => {
   const checkboxDiv = document.getElementById('category-checkbox');
   checkboxDiv.innerHTML = "";
   filteredSubCategories.forEach((subcategory) => {
-    checkboxDiv.insertAdjacentHTML('afterbegin', `<div class="form-check"><input type="checkbox" name="subcategories[]" value="${subcategory.id}"><label class="form-check-label">${subcategory.name}</label></div>` );
+    checkboxDiv.insertAdjacentHTML('afterbegin', `<div data-subcategory="${subcategory.id}" class="form-check rounded-card subcategory-card"><input type="checkbox" data-input="${subcategory.id}" name="subcategories[]" value=${subcategory.id} hidden="true"><label class="form-check-label">${subcategory.name}</label></div>` );
+  });
+  const subcategoryCheckboxes = document.querySelectorAll('.subcategory-card');
+  subcategoryCheckboxes.forEach((subcategoryCheckbox) => {
+    subcategoryCheckbox.addEventListener('click', (event) => {
+      const selectedSubcategory = event.currentTarget;
+      selectedSubcategory.classList.toggle('subcategory-card-selected');
+      selectedSubcategory.classList.toggle('subcategory-card');
+      const id = event.currentTarget.dataset.subcategory;
+      const checkbox = document.querySelector(`[data-input="${id}"]`);
+      if (checkbox.checked === true) {
+        checkbox.checked = false;
+      } else {
+        checkbox.checked = true;
+      }
+    });
   });
 }
 
